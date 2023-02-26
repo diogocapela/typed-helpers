9eb03603c4e23b528bff50bb2c0362193b3cf622
Diogo Capela <diogocapela@gmail.com>
2023-02-26 22:41:59 +0000

    Add ESLint static code analysis

--- src/lib/typedObject.spec.ts
+++ src/lib/typedObject.spec.ts
@@ -14,9 +14,11 @@ const originalKeys = Object.keys(planets);
 const typedKeys = typedObject.keys(planets);
 
 // @ts-expect-no-error
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const originalKey: (typeof originalKeys)[number] = 'potato';
 
 // @ts-expect-error
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const typedKey: (typeof typedKeys)[number] = 'potato';
 
 /**
@@ -27,7 +29,9 @@ const originalEntries = Object.entries(planets);
 const typedEntries = typedObject.entries(planets);
 
 // @ts-expect-no-error
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const originalEntry: (typeof originalEntries)[number][0] = 'potato';
 
 // @ts-expect-error
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const typedEntry: (typeof typedEntries)[number][0] = 'potato';
